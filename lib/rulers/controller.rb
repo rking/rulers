ad0991da96c99865309391f530cbaf77be275d9d
☈king <rking@sharpsaw.org>
2012-10-29 02:54:45 -0500

    Page 47

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -7,11 +7,15 @@ module Rulers
     attr_reader :env
 
     def render view_name, locals = {}
-      filename = "app/views/#{view_name}.html.erb"
+      filename = "app/views/#{controller_name}/#{view_name}.html.erb"
       template = File.read filename
       eruby = Erubis::Eruby.new template
       eruby.result locals.merge env: env
     end
 
+    def controller_name
+      klass = self.class.to_s.sub /Controller$/, ''
+      Rulers.to_underscore klass
+    end
   end
 end
