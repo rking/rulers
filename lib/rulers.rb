05c419bf17c347e22790bef6370da0019875c2f7
☈king <rking@sharpsaw.org>
2012-10-29 02:04:49 -0500

    Page 28

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,6 +4,9 @@ require 'rulers/routing'
 module Rulers
   class Application
     def call env
+      return [404, {'Content-type' => 'text/html'}, []] if
+        '/favicon.ico' == env['PATH_INFO']
+
       klass, act = get_controller_and_action env
       controller = klass.new env
       [200, {'Content-Type' => 'text/html'}, [controller.send(act)]]
