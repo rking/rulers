89a824b73ccdfe71805bfcc92ef1a63295f53bfe
☈king <rking@sharpsaw.org>
2012-12-23 18:08:33 -0600

    Compacting.

--- lib/rulers/file_model.rb
+++ lib/rulers/file_model.rb
@@ -1,25 +1,17 @@
 module Rulers
   module Model
     class FileModel
-      def initialize path
-        @path = path
-        # Note: Page 52 makes this excessively verbose:
-        @id = File.basename(path, '.yml').to_i
-        # Note: Page 52 has no sanity check on this, it seems. Sort of fixing:
-        actual_path = "db/quotes/#{@id}.yml"
-        @hash = YAML.load_file actual_path
+      def initialize id
+        @id = id.to_i
+        @path = "db/quotes/#{@id}.yml"
+        @hash = YAML.load_file @path
       end
 
       def [] key; @hash[key.to_s] end
       def []= key, val; @hash[key] = val end
 
       def self.find id
-        redundant_against_above = "db/quotes/#{id}.yml"
-        FileModel.new redundant_against_above
-      rescue
-        p $!
-        raise "FileModel for #{id} not found"
-        # Note: page 53 says this will return nil when not found. It won't.
+        FileModel.new id
       end
     end
   end
